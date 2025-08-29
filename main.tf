8b82a9d60fa4daf2558d7cde8b72dc2a947d671f
Daryl Swartz <113477545+dswartz007@users.noreply.github.com>
2025-08-29 09:28:59 -0400

    Refactor locals and remove random suffix resource
    Removed random string resource and updated locals to use variables.

--- main.tf
+++ main.tf
@@ -47,29 +47,15 @@ provider "azurerm" {
 data "azurerm_client_config" "current" {}
 
 ############################################################
-# Random suffix for uniqueness
-############################################################
-
-resource "random_string" "suffix" {
-  length  = 5
-  upper   = false
-  lower   = true
-  numeric = true
-  special = false
-}
-
-############################################################
 # Locals
 ############################################################
 
 locals {
-  base_prefix = var.name_prefix != "" ? var.name_prefix : "adfcmk-${random_string.suffix.result}"
-
-  key_vault_name       = substr(replace("${local.base_prefix}kv", "/[-_]/", ""), 0, 24)
-  resource_group_name  = "${local.base_prefix}-rg"
-  identity_name        = "${local.base_prefix}-id"
-  adf_name             = "${local.base_prefix}-adf"
-  key_name             = "adf-cmk"
+  key_vault_name       = var.key_vault_name
+  resource_group_name  = var.resource_group_name
+  identity_name        = var.identity_name
+  adf_name             = var.data_factory_name
+  key_name             = "adf-cmk-az01"
 }
 
 ############################################################
